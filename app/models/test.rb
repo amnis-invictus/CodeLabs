0881a0810f1ad0657d891ee7bd66a1e9b7f85fa4
Just Me <just806me@gmail.com>
2018-07-03 15:08:55 +0300

    fixed formatting

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   validates :test_num, presence: true
 
   belongs_to :problem
-  
+
   has_one_attached :input
 
   has_one_attached :answer
--- db/migrate/20180703115032_create_tests.rb
+++ db/migrate/20180703115032_create_tests.rb
@@ -2,10 +2,9 @@ class CreateTests < ActiveRecord::Migration[5.2]
   def change
     create_table :tests do |t|
       t.string :test_num
+      t.belongs_to :problem, foregin_key: true, index: true, null: false
 
-	  t.belongs_to :problem, foregin_key: true, index: true, null: false
-      
-      t.timestamps	  
-	end
+      t.timestamps
+    end
   end
 end
