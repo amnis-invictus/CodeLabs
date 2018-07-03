b4cf5a9cbbe2dd03f2d1d6641fc4b7007e121f1e
Just Me <just806me@gmail.com>
2018-07-03 15:52:51 +0300

    added has_many :tests to Problem

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,6 @@
 class Problem < ApplicationRecord
+  has_many :tests
+
   has_many :translations, class_name: 'ProblemTranslation'
 
   has_one :translation, -> { where language: I18n.locale }, class_name: 'ProblemTranslation'
