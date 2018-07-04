2c6ecc2fa7b29428dfa32cb9456beebd9d3532ad
Just Me <just806me@gmail.com>
2018-07-04 16:24:41 +0300

    splitted translations into several files

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,4 +2,6 @@ Rails.application.configure do
   config.i18n.available_locales = %i(ru en)
 
   config.i18n.default_locale = :ru
+
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 end
