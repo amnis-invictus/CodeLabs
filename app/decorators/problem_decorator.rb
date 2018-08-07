493a731a57b6abb8f26bddf97f80b40de768c9f9
Just Me <just806me@gmail.com>
2018-08-07 12:35:43 +0300

    some fixes

--- app/decorators/problem_decorator.rb
+++ app/decorators/problem_decorator.rb
@@ -15,20 +15,14 @@ class ProblemDecorator < Draper::Decorator
       {
         id: id,
         updated_at: updated_at,
-        checker_compiler_id: checker_compiler_id
-        checker_url: nil,
-        tests: tests,
-        memory_limit: memory_limit,
-        time_limit: time_limit
+        checker_compiler_id: checker_compiler_id,
+        checker_source_url: checker_source_url,
+        tests: tests
       }
     end
   end
 
-  def memory_limit
-    memory_limit * compiler.memory_a + compiler.memory_b
-  end
-
-  def time_limit
-    time_limit * compiler.time_a + compiler.time_b
+  def checker_source_url
+    h.url_for checker_source if checker_source.attached?
   end
 end
--- app/decorators/submission_decorator.rb
+++ app/decorators/submission_decorator.rb
@@ -7,14 +7,24 @@ class SubmissionDecorator < Draper::Decorator
     {
       id: id,
       compiler_id: compiler_id,
-      problem: problem
+      problem: problem,
       source_url: source_url,
       test_state: test_state,
-      fails_count: fails_count
+      fails_count: fails_count,
+      memory_limit: memory_limit,
+      time_limit: time_limit
     }
   end
 
   def source_url
     h.url_for source if source.attached?
+  end
+
+  def memory_limit
+    problem.memory_limit * compiler.memory_a + compiler.memory_b
+  end
+
+  def time_limit
+    problem.time_limit * compiler.time_a + compiler.time_b
   end
 end
