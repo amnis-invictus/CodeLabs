39f1c4e1d11f537fa955c7c0d6f22ea4d6cb62d8
Just Me <just806me@gmail.com>
2018-08-07 12:40:21 +0300

    optimized GET /api/submissions

--- app/controllers/api/submissions_controller.rb
+++ app/controllers/api/submissions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SubmissionsController < Api::ApplicationController
   private
   def collection
-    @collection ||= Submission.pending.with_attached_source
+    @collection ||= Submission.pending.with_attached_source.includes :problem, :compiler
   end
 end
