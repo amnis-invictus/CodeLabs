1d7a01caef1648d01d7ac670cf8470e8cca59106
Just Me <just806me@gmail.com>
2018-09-01 03:31:45 +0300

    fix csp and websockets

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,6 +6,7 @@ if Rails.env.production?
     policy.object_src  :none
     policy.script_src  :self, :https
     policy.style_src   :self, :https
+    policy.connect_src :self, :https, "wss://#{ ENV['HOST'] }"
   end
 
   Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
