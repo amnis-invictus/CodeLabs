95a0e5f741f1d7c7fff87fa4a37ae3977fb043c1
Just Me <just806me@gmail.com>
2018-09-01 03:38:16 +0300

    allow inline styles

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,8 +5,8 @@ if Rails.env.production?
     policy.img_src     :self, :https, :data
     policy.object_src  :none
     policy.script_src  :self, :https
-    policy.style_src   :self, :https
-    policy.connect_src :self, :https, "wss://#{ ENV['HOST'] }"
+    policy.style_src   :self, :https, :unsafe_inline
+    policy.connect_src :self, :https, :wss
   end
 
   Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
