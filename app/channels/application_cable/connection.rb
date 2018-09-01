841d5eee77324ca2d61bb003491465d56e5c08cc
Just Me <just806me@gmail.com>
2018-09-01 03:06:51 +0300

    configured websocket connection authorization

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,15 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      reject_unauthorized_connection unless current_user
+    end
+
+    def current_user
+      return unless cookies.encrypted[:auth_token]
+
+      @current_user ||= User.joins(:auth_tokens).find_by(auth_tokens: { id: cookies.encrypted[:auth_token] })
+    end
   end
 end
