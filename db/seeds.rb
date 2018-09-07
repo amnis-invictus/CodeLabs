2f64859dbb88616c3062df7505c64f5c09de9aa4
Just Me <just806me@gmail.com>
2018-09-07 11:39:31 +0300

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-Compiler.create id: 1, name: 'gcc', version: '6.3.0-1'
+# frozen_string_literal: true
+
+Compiler.create id: 1, name: 'gcc', version: '6.3.0-1', memory_a: 1, memory_b: 0, time_a: 1, time_b: 0
