1e906d533036c0da067eac4b0afbbbbffc34ba82
Just Me <just806me@gmail.com>
2018-09-09 16:19:29 +0300

    fixed 204 response code in error case

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -1,6 +1,6 @@
 class Api::LogsController < Api::ApplicationController
   def create
-    render :errors, status: 422 unless resource.save
+    render :errors, status: 422 and return unless resource.save
 
     head 204
   end
