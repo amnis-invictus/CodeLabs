3d169e43dac54ec117921257bc8ca49f08350b0a
Just Me <just806me@gmail.com>
2018-09-17 22:24:38 +0300

    added language selection menu

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
-  config.i18n.available_locales = %i(ru en)
+  config.i18n.available_locales = %i(ru en uk)
 
-  config.i18n.default_locale = :ru
+  config.i18n.default_locale = :uk
 
   config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 end
