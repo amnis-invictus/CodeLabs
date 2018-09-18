7e58508355b5842ef1d95be7f2024523419150ae
mike <nmish2005@gmail.com>
2018-09-18 22:23:32 +0300

    Adding admin form

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,14 @@
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    if (isNaN(parseInt(memory.value)))
+        return alert('В поле "Память" введено не число');
+
+    if (isNaN(parseInt(time.value)))
+        return alert('В поле "Время" введено не число');
+
+    if (isNaN(parseInt(rtime.value)))
+        return alert('В поле "Настоящее время" введено не число');
+
+    form.submit();
+};
