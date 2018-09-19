189eb26bb0d7031fd48611eadefdf5f24d05fb8c
Just Me <just806me@gmail.com>
2018-09-19 15:59:09 +0300

    fixed ReferenceError: form is not defined

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,14 +1,18 @@
-form.onsubmit = function (e) {
-    e.preventDefault();
+//
+// ReferenceError: form is not defined
+//
 
-    if (isNaN(parseInt(memory.value)))
-        return alert('В поле "Память" введено не число');
+// form.onsubmit = function (e) {
+//     e.preventDefault();
 
-    if (isNaN(parseInt(time.value)))
-        return alert('В поле "Время" введено не число');
+//     if (isNaN(parseInt(memory.value)))
+//         return alert('В поле "Память" введено не число');
 
-    if (isNaN(parseInt(rtime.value)))
-        return alert('В поле "Настоящее время" введено не число');
+//     if (isNaN(parseInt(time.value)))
+//         return alert('В поле "Время" введено не число');
 
-    form.submit();
-};
+//     if (isNaN(parseInt(rtime.value)))
+//         return alert('В поле "Настоящее время" введено не число');
+
+//     form.submit();
+// };
