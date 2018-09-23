37aef916448de28e4db80a006970d91cbec2ea2b
dima <vindiamond0@gmail.com>
2018-09-23 18:40:30 +0300

    filtering submissions

---
+++ app/assets/javascripts/sort.js
@@ -0,0 +1,65 @@
+if (typeof sortByUser !== 'undefined') {
+    window.addEventListener('load', () => {
+        var usernames = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            prefetch: {
+                url: 'test/users.json'
+            },
+        });
+        usernames.initialize();
+
+        $('#sortByUser').tagsinput({
+            typeaheadjs: {
+                name: 'usernames',
+                displayKey: 'name',
+                valueKey: 'name',
+                source: usernames.ttAdapter()
+            }
+        });
+
+
+        var problems = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            prefetch: {
+                url: 'test/problems.json',
+                filter: function (list) {
+                    return $.map(list, function (cityname) {
+                        return {name: cityname};
+                    });
+
+                }
+            }
+        });
+        problems.initialize();
+
+        $('#sortByTask').tagsinput({
+            typeaheadjs: {
+                name: 'problems',
+                displayKey: 'name',
+                valueKey: 'name',
+                source: problems.ttAdapter()
+            }
+        });
+
+
+        var statuses = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            prefetch: {
+                url: 'test/statuses.json'
+            },
+        });
+        statuses.initialize();
+
+        $('#sortByStatus').tagsinput({
+            typeaheadjs: {
+                name: 'statuses',
+                displayKey: 'name',
+                valueKey: 'name',
+                source: statuses.ttAdapter()
+            }
+        });
+    });
+}
