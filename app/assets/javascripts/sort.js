6143cee7cbde148458bc07c66ced3dc28ee96ada
mike <nmish2005@gmail.com>
2018-09-26 22:00:00 +0300

    Fixing submission filter issues

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -6,51 +6,50 @@ function getRemote(remote_url) {
     }).responseText;
 }
 
-if (typeof sortByUser !== 'undefined') {
-    window.addEventListener('turbolinks:load', () => {
-        let local = {
-            users: null,
-            problems: null,
-            statuses: null
-        };
+typeof sortByUser !== 'undefined' &&
+$(document).on('turbolinks:load', () => {
+    let local = {
+        users: null,
+        problems: null,
+        statuses: null
+    };
 
-        local.users = JSON.parse(getRemote('/v2/tests/users.json'));
-        local.problems = JSON.parse(getRemote('/v2/tests/problems.json'));
-        local.statuses = JSON.parse(getRemote('/v2/tests/statuses.json'));
+    local.users = JSON.parse(getRemote('/v2/tests/users.json'));
+    local.problems = JSON.parse(getRemote('/v2/tests/problems.json'));
+    local.statuses = JSON.parse(getRemote('/v2/tests/statuses.json'));
 
-        const usernames = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.whitespace,
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            local: local.users
-        });
-
-        $('#sortByUser').typeahead(null, {
-            name: 'usernames',
-            source: usernames
-        });
-
-
-        const problems = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.whitespace,
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            local: local.problems
-        });
-
-        $('#sortByTask').typeahead(null, {
-            name: 'problems',
-            source: problems
-        });
-
-
-        const statuses = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.whitespace,
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            local: local.statuses
-        });
-
-        $('#sortByStatus').typeahead(null, {
-            name: 'statuses',
-            source: statuses
-        });
+    const usernames = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        local: local.users
     });
-}
+
+    $('#sortByUser').typeahead(null, {
+        name: 'usernames',
+        source: usernames
+    });
+
+
+    const problems = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        local: local.problems
+    });
+
+    $('#sortByTask').typeahead(null, {
+        name: 'problems',
+        source: problems
+    });
+
+
+    const statuses = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        local: local.statuses
+    });
+
+    $('#sortByStatus').typeahead(null, {
+        name: 'statuses',
+        source: statuses
+    });
+});
