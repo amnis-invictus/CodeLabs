89bc8f425be9194a99d3bd7c6b4249c24c5cb7bd
Just Me <just806me@gmail.com>
2018-09-30 00:06:48 +0300

    fixed compiler select

--- spec/models/compiler_spec.rb
+++ spec/models/compiler_spec.rb
@@ -20,14 +20,4 @@ RSpec.describe Compiler, type: :model do
   it { should validate_numericality_of :time_a }
 
   it { should validate_numericality_of :time_b }
-
-  describe '.visible' do
-    subject { described_class.visible }
-
-    let(:expected) { described_class.where visible: true }
-
-    its(:includes_values) { should eq expected.includes_values }
-
-    its(:to_sql) { should eq expected.to_sql }
-  end
 end
