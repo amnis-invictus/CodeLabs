b7490e4e78ca2a6fa14ea89388f1f03392c4e177
Just Me <just806me@gmail.com>
2018-09-29 20:20:26 +0300

    make last migrations reverseable, fix code style

--- db/migrate/20180929141405_change_users_skills.rb
+++ db/migrate/20180929141405_change_users_skills.rb
@@ -1,7 +1,7 @@
 class ChangeUsersSkills < ActiveRecord::Migration[5.2]
   def change
-    remove_column(:users, :skills)
+    remove_column :users, :skills, :string, array: true
 
-    add_column(:users, :skills, :string)
+    add_column :users, :skills, :string
   end
 end
--- db/migrate/20180929141465_change_compiler_status.rb
+++ db/migrate/20180929141465_change_compiler_status.rb
@@ -2,6 +2,6 @@ class ChangeCompilerStatus < ActiveRecord::Migration[5.2]
   def change
     add_column :compilers, :status, :integer, default: 0
 
-    remove_column :compilers, :visible
+    remove_column :compilers, :visible, :boolean
   end
 end
