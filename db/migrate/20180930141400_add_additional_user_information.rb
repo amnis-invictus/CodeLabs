89df5cbaf6f4cab53a4da203339062035653ddaa
Kostyuk K.A <kostyanf14@vtl.edu>
2018-09-30 14:46:58 +0300

    Added some additional user information in DB

---
+++ db/migrate/20180930141400_add_additional_user_information.rb
@@ -0,0 +1,13 @@
+class AddAdditionalUserInformation < ActiveRecord::Migration[5.2]
+  def change
+    enable_extension "pg_trgm"
+
+    add_column :users, :city, :string
+
+    add_index :users, :city, using: :gist, opclass: :gist_trgm_ops
+
+    add_column :users, :institution, :string
+
+    add_index :users, :institution, using: :gist, opclass: :gist_trgm_ops
+  end
+end
