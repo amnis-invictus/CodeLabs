4e973a61f00df5858abdb4e3767d6906e1a757c3
Just Me <just806me@gmail.com>
2018-10-13 12:35:17 +0300

    fixed search-select naming

--- app/assets/javascripts/search-select.js
+++ app/assets/javascripts/search-select.js
@@ -1,35 +1,35 @@
 'use strict';
 
 jQuery.fn.extend({
-    tagSelect: function tagSelect() {
-        return this.each(function () {
-            var block = $(this);
+  searchSelect: function () {
+    return this.each(function () {
+      var block = $(this);
 
-            var data = new Bloodhound({
-                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                prefetch: {url: block.data('path'), cache: false}
-            });
+      var data = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: { url: block.data('path'), cache: false }
+      });
 
-            data.initialize();
+      data.initialize();
 
-            block.tagsinput({
-                itemValue: 'value',
-                itemText: 'text',
-                typeaheadjs: {
-                    name: 'cities',
-                    displayKey: 'text',
-                    source: data.ttAdapter()
-                }
-            });
+      block.tagsinput({
+        itemValue: 'value',
+        itemText: 'text',
+        typeaheadjs: {
+          name: 'cities',
+          displayKey: 'text',
+          source: data.ttAdapter()
+        }
+      });
 
-            $.map(this.options, function (option) {
-                block.tagsinput('add', {value: option.value, text: option.text});
-            });
-        });
-    }
+      $.map(this.options, function (option) {
+        block.tagsinput('add', { value: option.value, text: option.text });
+      });
+    });
+  }
 });
 
 document.addEventListener('turbolinks:load', function () {
-    $('[data-role=search-select]').tagSelect();
+  $('[data-role=search-select]').searchSelect();
 });
