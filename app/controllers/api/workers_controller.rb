f74d23bd3ce650ec3feb33235679f6f410358f13
kostyanf14 <kostyanf14@live.com>
2018-10-14 13:37:49 +0300

    As a Worker I can sing up

---
+++ app/controllers/api/workers_controller.rb
@@ -0,0 +1,18 @@
+class Api::WorkersController < Api::ApplicationController
+  skip_before_action :authorize_resource, only: :create
+
+  def create
+    render :errors, status: 422 and return unless resource.save
+  end
+
+  private
+  attr_reader :resource
+
+  def resource_params
+    params.require(:worker).permit(:name, :api_version, :api_type, :webhook_supported, :status, ips: [])
+  end
+
+  def build_resource
+    @resource = Worker.new resource_params
+  end
+end
---
+++ spec/controllers/api/workers_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Api::WorkersController, type: :controller do
+  pending
+end
