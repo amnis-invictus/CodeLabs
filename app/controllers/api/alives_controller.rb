1733fc45352726efe0bca7458b0bd7a392ea0c44
Just Me <just806me@gmail.com>
2018-10-14 18:58:56 +0300

    rename parent to resource in Api AlivesController and SessionsController

--- app/controllers/api/alives_controller.rb
+++ app/controllers/api/alives_controller.rb
@@ -2,15 +2,15 @@ class Api::AlivesController < Api::ApplicationController
   skip_before_action :authorize_resource, :build_resource, only: :create
 
   def create
-    render :errors, status: 422 and return unless parent.update resource_params
+    render :errors, status: 422 and return unless resource.update resource_params
 
     head 204
   end
 
   private
 
-  def parent
-    @parent ||= Worker.find params[:worker_id]
+  def resource
+    @resource ||= Worker.find params[:worker_id]
   end
 
   def resource_params
--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,20 +2,20 @@ class Api::SessionsController < Api::ApplicationController
   skip_before_action :authorize_resource, :build_resource, only: %i(create destroy)
 
   def create
-    render :errors, status: 422 and return unless parent.update alive_at: Time.zone.now, status: :ok
+    render :errors, status: 422 and return unless resource.update alive_at: Time.zone.now, status: :ok
 
     head 204
   end
 
   def destroy
-    render :errors, status: 422 and return unless parent.update alive_at: Time.zone.now, status: :disabled, task_status: []
+    render :errors, status: 422 and return unless resource.update alive_at: Time.zone.now, status: :disabled, task_status: []
 
     head 204
   end
 
   private
 
-  def parent
-    @parent ||= Worker.find params[:worker_id]
+  def resource
+    @resource ||= Worker.find params[:worker_id]
   end
 end
