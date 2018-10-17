592346656d56de5543108b9e2764040a50cbbfce
Just Me <just806me@gmail.com>
2018-10-17 22:07:15 +0300

    updated ProblemPolicy for private Problem

--- app/policies/problem_policy.rb
+++ app/policies/problem_policy.rb
@@ -6,17 +6,13 @@ class ProblemPolicy < ApplicationPolicy
   def update?
     return false if user.blank?
 
-    return true if user.moderator?
-
-    user == resource.user
+    user.moderator? || user == resource.user
   end
 
   def destroy?
     return false if user.blank?
 
-    return true if user.moderator?
-
-    user == resource.user
+    user.moderator? || user == resource.user
   end
 
   def index?
@@ -24,6 +20,12 @@ class ProblemPolicy < ApplicationPolicy
   end
 
   def show?
-    true
+    return true unless resource.private?
+
+    return false if user.blank?
+
+    return true if user.moderator? || user == resource.user
+
+    user.shared_problems.include? resource
   end
 end
--- spec/policies/problem_policy_spec.rb
+++ spec/policies/problem_policy_spec.rb
@@ -61,7 +61,63 @@ RSpec.describe ProblemPolicy do
     it { should permit owner, resource }
   end
 
-  permissions :index?, :show? do
+  permissions :index? do
     it { should permit nil, double }
+  end
+
+  permissions :show? do
+    context do
+      let(:resource) { stub_model Problem, private: false }
+
+      it { should permit nil, resource }
+
+      it { should permit stub_model(User), resource }
+    end
+
+    context do
+      let(:owner) { stub_model User }
+
+      let(:resource) { stub_model Problem, private: true, user: owner }
+
+      it { should permit owner, resource }
+
+      context do
+        let(:user) { stub_model User, roles: [:moderator] }
+
+        it { should permit user, resource }
+      end
+
+      context do
+        let(:user) { stub_model User }
+
+        before { expect(user).to receive(:shared_problems).and_return([resource]) }
+
+        it { should permit user, resource }
+      end
+
+      context do
+        let(:user) { stub_model User }
+
+        before { expect(user).to receive(:shared_problems).and_return([]) }
+
+        it { should_not permit user, resource }
+      end
+
+      context do
+        let(:user) { stub_model User, roles: [:administrator] }
+
+        before { expect(user).to receive(:shared_problems).and_return([]) }
+
+        it { should_not permit user, resource }
+      end
+
+      context do
+        let(:user) { stub_model User, roles: [:confirmed] }
+
+        before { expect(user).to receive(:shared_problems).and_return([]) }
+
+        it { should_not permit user, resource }
+      end
+    end
   end
 end
