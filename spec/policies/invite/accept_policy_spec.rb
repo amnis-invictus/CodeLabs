95281b602c7285eed55bbfee90efb4964e819bc9
Just Me <just806me@gmail.com>
2018-10-17 20:19:55 +0300

    rework accept/reject policies again

--- spec/policies/invite/accept_policy_spec.rb
+++ spec/policies/invite/accept_policy_spec.rb
@@ -3,45 +3,35 @@ require 'rails_helper'
 RSpec.describe Invite::AcceptPolicy do
   subject { described_class }
 
-  fixtures :users
+  let(:sender) { stub_model User }
+
+  let(:receiver) { stub_model User }
+
+  let(:status) { :pending }
+
+  let(:invite) { stub_model Invite, status: status, sender: sender, receiver: receiver }
 
   let(:resource) { Invite::Accept.new invite }
 
   permissions :new?, :create? do
-    context do
-      let(:invite) { stub_model Invite, status: :pending }
+    it { should_not permit nil, resource }
 
-      it { should_not permit nil, resource }
+    it { should_not permit stub_model(User), resource }
+
+    it { should_not permit sender, resource }
+
+    it { should permit receiver, resource }
+
+    context do
+      let(:status) { :accepted }
+
+      it { should_not permit receiver, resource }
     end
 
     context do
-      let(:invite) { stub_model Invite, status: :pending, sender: users(:one), receiver: users(:two) }
+      let(:status) { :rejected }
 
-      it { should_not permit users(:one), resource }
-    end
-
-    context do
-      let(:invite) { stub_model Invite, status: :pending, sender: users(:one), receiver: users(:two) }
-
-      it { should_not permit users(:three), resource }
-    end
-
-    context do
-      let(:invite) { stub_model Invite, status: :accepted, sender: users(:one), receiver: users(:two) }
-
-      it { should_not permit users(:two), resource }
-    end
-
-    context do
-      let(:invite) { stub_model Invite, status: :rejected, sender: users(:one), receiver: users(:two) }
-
-      it { should_not permit users(:two), resource }
-    end
-
-    context do
-      let(:invite) { stub_model Invite, status: :pending, sender: users(:one), receiver: users(:two) }
-
-      it { should permit users(:two), resource }
+      it { should_not permit receiver, resource }
     end
   end
 end
