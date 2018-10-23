2a1985d526f422818be66cf73a2283d6b1a31ee5
Just Me <just806me@gmail.com>
2018-10-23 17:03:04 +0300

    skip authorization for /api/problems/:id, /api/submissions

--- app/controllers/api/problems_controller.rb
+++ app/controllers/api/problems_controller.rb
@@ -1,4 +1,6 @@
 class Api::ProblemsController < Api::ApplicationController
+  skip_before_action :authorize_collection, :authorize_resource
+
   private
   def resource
     @resource ||= Problem.find params[:id]
