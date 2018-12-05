3cf8f020ab3e5faa08b89207338ae25fb82b0afa
Just Me <just806me@gmail.com>
2018-12-05 20:30:53 +0200

    fixed revision initializer for non-existing file

--- config/initializers/revision.rb
+++ config/initializers/revision.rb
@@ -1,6 +1,6 @@
 Rails.application.config.revision = \
-  if Rails.env.production? && ENV.key?('REVISION_FILE')
+  if Rails.env.production? && ENV.key?('REVISION_FILE') && File.exist?(ENV['REVISION_FILE'])
     IO.readlines(ENV['REVISION_FILE']).last.strip
   else
-    'debug'
+    ''
   end.freeze
