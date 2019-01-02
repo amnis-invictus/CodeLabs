cce2d1a0894bb5f9b81af8dd4d598c50cbf81925
Just Me <just806me@gmail.com>
2019-01-02 23:20:41 +0200

    tried to fix CSP issues w/ Turbolinks

---
+++ app/assets/javascripts/turbolinks-persistent-csp-nonce.js
@@ -0,0 +1,11 @@
+(function () {
+  var nonce = undefined
+
+  document.addEventListener('turbolinks:load', function () {
+    if (nonce === undefined) {
+      nonce = $('meta[name="csp-nonce"]').prop('content')
+    } else {
+      $('meta[name="csp-nonce"]').prop('content', nonce)
+    }
+  })
+})()
