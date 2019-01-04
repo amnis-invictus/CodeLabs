a596f86f2e3da91af839283e137ee1ddcc9511fc
Just Me <just806me@gmail.com>
2019-01-04 18:13:04 +0200

    fixed cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,10 +10,6 @@ append :linked_files, '.env', 'config/master.key'
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle', 'storage'
 
-set :rbenv_type, :user
-
-set :rbenv_ruby, File.read('.ruby-version').strip
-
 set :deploy_to, '/home/user/application'
 
 namespace :deploy do
