4b5dcdd6ffdf0530827352148e7ab9ee92385a05
Just Me <just806me@gmail.com>
2019-01-23 15:07:58 +0200

    added missing specs for Log resource

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Log, type: :model do
+  it { expect(described_class.inheritance_column).to eq '' }
+
   it { should validate_presence_of :data }
 
   it { should define_enum_for(:type).with_values(source: 0, checker: 1) }
 
   it { should belong_to :submission }
-
-  it { expect(described_class.inheritance_column).to eq '' }
 end
--- spec/policies/log_policy_spec.rb
+++ spec/policies/log_policy_spec.rb
@@ -3,5 +3,32 @@ require 'rails_helper'
 RSpec.describe LogPolicy do
   subject { described_class }
 
-  pending
+  permissions :create? do
+    let(:resource) { stub_model Log, submission: stub_model(Submission, test_state: test_state) }
+
+    context do
+      let(:test_state) { :pending }
+
+      it { should_not permit nil, resource  }
+    end
+
+    context do
+      let(:test_state) { :in_progress }
+
+      it { should permit nil, resource  }
+    end
+
+    context do
+      let(:test_state) { :done }
+
+      it { should_not permit nil, resource  }
+    end
+
+    context do
+      let(:test_state) { :failed }
+
+      it { should_not permit nil, resource  }
+    end
+
+  end
 end
