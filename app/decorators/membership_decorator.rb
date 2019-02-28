0335ebabf9a360732628e7e7d162f15e4fe85d41
Just Me <just806me@gmail.com>
2019-02-28 11:05:45 +0200

    added group name to memberships table

--- app/decorators/membership_decorator.rb
+++ app/decorators/membership_decorator.rb
@@ -1,7 +1,9 @@
 class MembershipDecorator < Draper::Decorator
   delegate_all
 
-  decorates_association :user
+  decorates_associations :user, :group
 
   delegate :name, to: :user, prefix: true, allow_nil: true
+
+  delegate :name, to: :group, prefix: true, allow_nil: true
 end
