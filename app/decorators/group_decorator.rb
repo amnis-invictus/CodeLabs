d2ff19ac709042cf2603c927b91fadd6ee8453ac
Just Me <just806me@gmail.com>
2020-02-05 12:36:41 +0200

    improved group show action - less db requests
    Task: #80

--- app/decorators/group_decorator.rb
+++ app/decorators/group_decorator.rb
@@ -5,6 +5,8 @@ class GroupDecorator < Draper::Decorator
 
   delegate :name, to: :owner, prefix: true
 
+  delegate :state_requested?, :state_invited?, :state_accepted?, to: :current_user_membership, prefix: true, allow_nil: true
+
   def visibility_icon
     h.content_tag :i, '', class: visibility_icon_class, title: visibility.humanize,
       data: { toggle: :tooltip, placement: :bottom }
@@ -19,5 +21,9 @@ class GroupDecorator < Draper::Decorator
     when :public
       'mr-3 fas fa-unlock'
     end
+  end
+
+  def current_user_membership
+    @current_user_membership ||= memberships.find_by user: h.current_user if h.current_user.present?
   end
 end
