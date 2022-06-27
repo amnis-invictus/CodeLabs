5cd07d28d643ca2085c5b329ec4e758b96979398
Just Me <just806me@gmail.com>
2022-06-27 14:47:10 +0300

    bump rails from 5.2.8 to 6.0.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module CodeLabs # rubocop:disable Style/ClassAndModuleChildren
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.time_zone = 'Kyiv'
     config.active_record.default_timezone = :local
 
