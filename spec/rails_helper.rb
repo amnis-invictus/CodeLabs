b0bb27a6601d582a5ab5b61fe8af95457e9135aa
Kostiantyn Kostiuk <kostyanf14@live.com>
2022-08-03 22:47:34 +0300

    test: Add Capybara RSpec skeleton
    Export UI_TEST_ENGINE environment variable to select chrome as a selenium
    web driver. Firefox is a default.
    
    Export UI_TEST_HEADLESS or CI environment variable to run selenium
    in headless mode.
    
    Export UI_TEST_SILENT_PUMA to run puma server in silent mode (do
    not print server information).
    
    Export SELENIUM_CUSTOM_URL to run Capybara with custom selenium server
    Example
      docker run -d --rm -p 4444:4444/tcp --net=host selenium/standalone-firefox:95.0
      export SELENIUM_CUSTOM_URL = 'http://localhost:4444/wd/hub'
    
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>
    Co-authored-by: Just Me <just806me@gmail.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'pundit/rspec'
 require 'aasm/rspec'
+require 'capybara/rspec'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
@@ -40,6 +41,20 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
 
     with.library :rails
+  end
+end
+
+browser = ENV['UI_TEST_ENGINE']&.downcase == 'chrome' ? :chrome : :firefox
+driver = 'selenium'
+driver = "#{ driver }_chrome" if browser == :chrome
+driver = "#{ driver }_headless" if ENV.fetch('UI_TEST_HEADLESS') { ENV.fetch('CI', nil) }
+
+Capybara.default_driver = driver.to_sym
+Capybara.server = :puma, { Silent: true } if ENV['UI_TEST_SILENT_PUMA']
+
+if ENV['SELENIUM_CUSTOM_URL']
+  Capybara.register_driver driver.to_sym do |app|
+    Capybara::Selenium::Driver.new app, browser: browser, url: ENV.fetch('SELENIUM_CUSTOM_URL')
   end
 end
 
