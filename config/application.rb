ffcd5df247b2bd80da9c1bda592bddc26d00dae2
Kostiantyn Kostiuk <kostyanf14@live.com>
2022-08-06 18:16:42 +0300

    config: Set factory bot options when it avaliable
    Factory bot is in development/test group, so we can't update
    it in production.
    
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module CodeLabs # rubocop:disable Style/ClassAndModuleChildren
     config.time_zone = 'Kyiv'
     config.active_record.default_timezone = :local
 
-    config.factory_bot.definition_file_paths = ['spec/factory_bot']
+    config.factory_bot.definition_file_paths = ['spec/factory_bot'] if defined?(FactoryBotRails)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
