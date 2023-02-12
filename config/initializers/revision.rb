72ff0d3432c0c50c37c33093c9db203d4d526345
Just Me <just806me@gmail.com>
2023-02-12 17:08:11 +0200

    Fix if revision file is missing

--- config/initializers/revision.rb
+++ config/initializers/revision.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   path = ENV.fetch 'REVISION_FILE'
-  REVISION = File.readlines(path).last.strip.freeze
+  REVISION = File.exist?(path) ? File.readlines(path).last.strip.freeze : ''.freeze
 else
   REVISION = "<#{ Rails.env }> revision".freeze
 end
