43d52e9668967b051de53b4133813ded413e4a7d
Just Me <just806me@gmail.com>
2024-02-19 21:15:41 +0200

    Add expectation for page load in login helper

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,9 +1,8 @@
 module LoginHelper
   def perform_login email, password
     visit '/en/session/new'
-
     fill_inputs 'session', { email: email, password: password }
-
     click_button 'commit'
+    expect(page).to have_current_path(profile_path(I18n.locale))
   end
 end
